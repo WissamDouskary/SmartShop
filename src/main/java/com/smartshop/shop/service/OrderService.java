1d92bb954eb03ab0ddb35c42b329f3a1ce08528f
WissamDouskary <douskary.wissam@gmail.com>
2025-11-25 16:17:28 +0100

    SMR-10 create validation of product stock and create method to decrement stock after add them to orders

--- src/main/java/com/smartshop/shop/service/OrderService.java
+++ src/main/java/com/smartshop/shop/service/OrderService.java
@@ -1,9 +1,14 @@
 package com.smartshop.shop.service;
 
+import com.smartshop.shop.dto.requestDTO.OrderItemRequestDTO;
 import com.smartshop.shop.dto.responseDTO.OrderResponseDTO;
+import com.smartshop.shop.exception.BusinessException;
 import com.smartshop.shop.mapper.OrderMapper;
 import com.smartshop.shop.model.Order;
+import com.smartshop.shop.model.OrderItem;
+import com.smartshop.shop.model.Product;
 import com.smartshop.shop.repository.OrderRepository;
+import com.smartshop.shop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +21,7 @@ import java.util.stream.Collectors;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final OrderMapper orderMapper;
+    private final ProductRepository productRepository;
 
     @Transactional(readOnly = true)
     public List<OrderResponseDTO> getOrdersByClient(String clientId) {
@@ -24,5 +30,32 @@ public class OrderService {
         return orders.stream()
                 .map(orderMapper::toResponse)
                 .collect(Collectors.toList());
+    }
+
+    private void validateStockAvailability(List<OrderItemRequestDTO> items) {
+        for (OrderItemRequestDTO item : items) {
+            Product product = productRepository.findById(item.getProductId())
+                    .orElseThrow(() -> new BusinessException("Product not found"));
+
+            if (product.getStockDisponible() < item.getQuantity()) {
+                throw new BusinessException("Insufficient stock for product: " + product.getNom());
+            }
+        }
+    }
+
+    @Transactional
+    public void decrementStock(Order order) {
+        for (OrderItem item : order.getOrderItemList()) {
+            Product product = item.getProduct();
+
+            if (product.getStockDisponible() < item.getQuantity()) {
+                throw new BusinessException("Stock changed! Insufficient stock for: " + product.getNom());
+            }
+
+            int newStock = product.getStockDisponible() - item.getQuantity();
+            product.setStockDisponible(newStock);
+
+            productRepository.save(product);
+        }
     }
 }
